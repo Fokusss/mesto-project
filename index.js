6f3be79620fd75960ea45f495247fe160a483230
Arseniy Sazonov <rimus00@yandex.ru>
2022-05-20 02:33:40 +0300

    Добавил шесть мест из js. Так же добавил возможность добавить место по нажатию на кнопку

--- index.js
+++ index.js
@@ -4,44 +4,137 @@
   Отключает стандартное поведение формы.
 */
 
-/* Функиция открытия и закрытия попапа */
+/* Функиция открытия и закрытия попапа профиля */
 
-let popUpBodyList = document.querySelector('.pop-up_el_profile-edit');
-let editOpenButton = document.querySelector('.profile__edit');
-let editCloseButton = document.querySelector('.pop-up__close');
-let editSaveButton = document.querySelector('.pop-up__save');
+let buttonOpenEditProfile = document.querySelector(".profile__edit");
+let buttonOpenAddCard = document.querySelector(".profile__add");
+let popUpProfile = document.querySelector(".pop-up_el_profile-edit");
+let buttonCloseEditProfile = popUpProfile.querySelector(".pop-up__close");
+let buttonSubmitEditProfile = popUpProfile.querySelector(".pop-up__form");
+let popUpAddCard = document.querySelector(".pop-up_el_add-card");
+let buttonCloseAddCard = popUpAddCard.querySelector(".pop-up__close");
+let buttonSubmitAddCard = popUpAddCard.querySelector(".pop-up__form");
+let photoGrid = document.querySelector(".photo__grid");
 
+const urlPlace = ['./images/milandi.jpg', './images/Mombasa.jpg', './images/Kilwa.jpg',
+  './images/puntaarneas.jpg', './images/tenerife.jpg', './images/sevilya.jpg'];
 
-function openProfileEdit() {
-  let profileName = document.querySelector('.profile__name').textContent;
-  let profileText = document.querySelector('.profile__text').textContent;
-  let inputName = document.querySelector('.pop-up__input_el_name');
-  let inputText = document.querySelector('.pop-up__input_el_text');
-  inputName.setAttribute('placeholder', `${profileName}`);
-  inputText.setAttribute('placeholder', `${profileText}`);
-  popUpBodyList.classList.add('pop-up_active');
+const namePlace = ['Миланди', 'Момбаса', 'Килва-Кисивани', 'Пунта-Аренас', 'Тенерифе', 'Севилья'];
+
+function changePlaceholder(element) {
+  /* Коректные данные плейсхолдера */
+  let name = document.querySelector(".profile__name").textContent;
+  let text = document.querySelector(".profile__text").textContent;
+  element
+    .querySelector(".pop-up__input_el_name")
+    .setAttribute("placeholder", `${name}`);
+  element
+    .querySelector(".pop-up__input_el_text")
+    .setAttribute("placeholder", `${text}`);
 }
 
-editOpenButton.addEventListener('click', openProfileEdit);
-
-function closeProfileEdit() { // Функция закрытия Попапа
-  popUpBodyList.classList.remove('pop-up_active');
+function changeProfile(element) {
+  /* Меняет данные в профиле */
+  let nameForm = element.querySelector(".pop-up__input_el_name").value;
+  let textForm = element.querySelector(".pop-up__input_el_text").value;
+  document.querySelector(".profile__name").textContent = nameForm;
+  document.querySelector(".profile__text").textContent = textForm;
 }
 
-editCloseButton.addEventListener('click', closeProfileEdit);
-
-function saveProfileEdit(evt) {
-  evt.preventDefault()
-  let inputValueName = document.querySelector('.pop-up__input_el_name').value;
-  let inputValueText = document.querySelector('.pop-up__input_el_text').value;
-  if (inputValueName !== '' && inputValueText !== '') {
-    document.querySelector('.profile__name').textContent = inputValueName;
-    document.querySelector('.profile__text').textContent = inputValueText;
-    closeProfileEdit()
-    document.querySelector('.pop-up__input_el_name').value = '';
-    document.querySelector('.pop-up__input_el_text').value = '';
+function deleteValueInput(element) {
+  /* Удаляет из инпута значение */
+  let allInput = element.querySelectorAll(".pop-up__input");
+  for (let i = 0; allInput.length !== i; i += 1) {
+    allInput[i].value = "";
   }
 }
 
-editSaveButton.addEventListener('click', saveProfileEdit);
+function hidePopUp(element) {
+  /* Закрытие окна */
+  element.classList.remove("pop-up_active");
+}
 
+function openPopUp(element) {
+  /* Открытие окна */
+  element.classList.add("pop-up_active");
+}
+
+function addCard(element) {
+  /* Добавление мест */
+  let nameCard = element.querySelector(".pop-up__input_el_mesto").value;
+  let urlImage = element.querySelector(".pop-up__input_el_image-url").value;
+  photoGrid.insertAdjacentHTML(
+    "afterbegin",
+    `<li>
+  <div class="card">
+    <img
+      src="${urlImage}"
+      alt="фото места"
+      class="card__image"
+    />
+    <div class="card__down">
+      <h2 class="card__name">${nameCard}</h2>
+      <button aria-label="like" type="button" class="card__like button"></button>
+    </div>
+  </div>
+  </li>`
+  );
+}
+
+function addCards(namePlace, urlPlace) {
+  for (let i = 0; namePlace.length !== i; i += 1) {
+    photoGrid.insertAdjacentHTML(
+      "afterbegin",
+      `<li>
+  <div class="card">
+    <img
+      src="${urlPlace[i]}"
+      alt="фото места"
+      class="card__image"
+    />
+    <div class="card__down">
+      <h2 class="card__name">${namePlace[i]}</h2>
+      <button aria-label="like" type="button" class="card__like button"></button>
+    </div>
+  </div>
+  </li>`
+    );
+  }
+}
+
+/* Слушатели "Профиля" */
+
+buttonOpenEditProfile.addEventListener("click", function () {
+  openPopUp(popUpProfile);
+  changePlaceholder(popUpProfile);
+});
+
+buttonCloseEditProfile.addEventListener("click", function () {
+  hidePopUp(popUpProfile);
+});
+
+buttonSubmitEditProfile.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  changeProfile(popUpProfile);
+  deleteValueInput(popUpProfile);
+  hidePopUp(popUpProfile);
+});
+
+/* Слушатели добавления места */
+
+buttonOpenAddCard.addEventListener("click", function () {
+  openPopUp(popUpAddCard);
+});
+
+buttonCloseAddCard.addEventListener("click", function () {
+  hidePopUp(popUpAddCard);
+});
+
+buttonSubmitAddCard.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  addCard(popUpAddCard);
+  deleteValueInput(popUpAddCard);
+  hidePopUp(popUpAddCard);
+});
+
+addCards(namePlace, urlPlace);
